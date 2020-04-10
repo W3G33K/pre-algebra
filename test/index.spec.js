a6c2868788fd0faefaf0570808c3e5bb847cb736
Ryan K. Clark <mail.w3geek@gmail.com>
2020-04-10 17:40:19 -0400

    It'd be fun, they said.

---
+++ test/index.spec.js
@@ -0,0 +1,11 @@
+/** @imports */
+const chai = require("chai");
+
+/** @globals */
+const expect = chai.expect;
+
+describe("Behavior Driven Design Spec", function() {
+	it("true is true", function() {
+		expect(true, "abs(boolshit)").to.be.true;
+	});
+});

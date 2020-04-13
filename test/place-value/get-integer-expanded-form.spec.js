e082f5fbb4959470fb1535060020f473359a55e0
Ryan K. Clark <mail.w3geek@gmail.com>
2020-04-13 10:37:25 -0400

    task/writing-a-number-in-expanded-form > Iteration 12

--- test/place-value/get-integer-expanded-form.spec.js
+++ test/place-value/get-integer-expanded-form.spec.js
@@ -16,7 +16,8 @@ const parameterizedTests = [
 	{actual: 6, expected: "six"}, {actual: 7, expected: "seven"}, {actual: 8, expected: "eight"},
 	{actual: 9, expected: "nine"}, {actual: 10, expected: "ten"}, {actual: 15, expected: "fifteen"},
 	{actual: -13, expected: "negative thirteen"}, {actual: -16, expected: "negative sixteen"}, {actual: -18, expected: "negative eighteen"},
-	{actual: 20, expected: "twenty"}, {actual: 90, expected: "ninety"}
+	{actual: 20, expected: "twenty"}, {actual: 90, expected: "ninety"}, {actual: 50, expected: "fifty"},
+	{actual: -30, expected: "negative thirty"}, {actual: -40, expected: "negative forty"}, {actual: -70, expected: "negative seventy"}
 ];
 
 describe("Get Integer Expanded Form", function() {

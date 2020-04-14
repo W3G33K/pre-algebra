05c3440944619598dba7fb92ad0c4a78485a91c8
Ryan K. Clark <mail.w3geek@gmail.com>
2020-04-14 18:15:05 -0400

    task/writing-a-number-in-expanded-form > Iteration 21

--- test/place-value/get-integer-expanded-form.spec.js
+++ test/place-value/get-integer-expanded-form.spec.js
@@ -32,7 +32,10 @@ const parameterizedTests = [
 	{actual: -1001, expected: "negative one thousand one"}, {actual: -5001, expected: "negative five thousand one"}, {actual: -9001, expected: "negative nine thousand one"},
 	{actual: 4321, expected: "four thousand three hundred twenty one"}, {actual: 1234, expected: "one thousand two hundred thirty four"}, {actual: 5678, expected: "five thousand six hundred seventy eight"},
 	{actual: -9764, expected: "negative nine thousand seven hundred sixty four"}, {actual: -5555, expected: "negative five thousand five hundred fifty five"}, {actual: -3456, expected: "negative three thousand four hundred fifty six"},
-	{actual: 50000, expected: "fifty thousand"}
+	{actual: 50000, expected: "fifty thousand"}, {actual: 70000, expected: "seventy thousand"}, {actual: 90000, expected: "ninety thousand"},
+	{actual: -10000, expected: "negative ten thousand"}, {actual: -30000, expected: "negative thirty thousand"}, {actual: -50000, expected: "negative fifty thousand"},
+	{actual: 55555, expected: "fifty five thousand five hundred fifty five"}, {actual: 76543, expected: "seventy six thousand five hundred forty three"}, {actual: 98765, expected: "ninety eight thousand seven hundred sixty five"},
+	{actual: -34567, expected: "negative thirty four thousand five hundred sixty seven"}, {actual: -12345, expected: "negative twelve thousand three hundred forty five"}, {actual: -56473, expected: "negative fifty six thousand four hundred seventy three"}
 ];
 
 describe("Get Integer Expanded Form", function() {
